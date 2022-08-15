f457a02969d90435479f0512a3e87111edb511ff
Pilladian <ph-zimm-acc@pm.me>
2022-08-15 13:09:03 +0200

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module main
+
+go 1.19
+
+require github.com/Pilladian/logger v1.1.1 // indirect
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/Pilladian/logger"
+
+func main() {
+	logger.SetLogLevel(2)
+	logger.Info("----- Starting InfoWatch Server -----")
+}
