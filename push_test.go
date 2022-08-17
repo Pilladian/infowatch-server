44e11f396929bfd01992120691de09b2c2826f4e
Pilladian <ph-zimm-acc@pm.me>
2022-08-17 12:43:37 +0200

    test case for #5

--- push_test.go
+++ push_test.go
@@ -2,18 +2,43 @@ package main
 
 import (
 	"errors"
+	"math/rand"
 	"testing"
+	"time"
 )
 
 func TestProcessData_EmptyID(t *testing.T) {
-	got, err_got := processData("", "{'test': 'test'}")
+	rand.Seed(time.Now().UnixNano())
+	initialize()
+
+	got, err_got := processData("", "{\"test\": \"test\"}")
 	want := 805
 	err_want := errors.New("Unrecognized ID format \"\"")
 
 	if got != want {
 		t.Errorf("[ WANTED ] %d\n\t     [ ACTUAL ] %d\n\n", want, got)
 	}
-	if err_got.Error() != err_want.Error() {
+	if err_got == nil {
+		t.Errorf("[ WANTED ] %s\n\t     [ ACTUAL ] <nil>", err_want.Error())
+	} else if err_got.Error() != err_want.Error() {
+		t.Errorf("[ WANTED ] %s\n\t     [ ACTUAL ] %s", err_want.Error(), err_got.Error())
+	}
+}
+
+func TestProcessData_UnknownJsonType(t *testing.T) {
+	rand.Seed(time.Now().UnixNano())
+	initialize()
+
+	got, err_got := processData("test_"+randomString(10), "{\"test\": -}")
+	want := 806
+	err_want := errors.New("Unknown type for json data")
+
+	if got != want {
+		t.Errorf("[ WANTED ] %d\n\t     [ ACTUAL ] %d\n\n", want, got)
+	}
+	if err_got == nil {
+		t.Errorf("[ WANTED ] %s\n\t     [ ACTUAL ] <nil>", err_want.Error())
+	} else if err_got.Error() != err_want.Error() {
 		t.Errorf("[ WANTED ] %s\n\t     [ ACTUAL ] %s", err_want.Error(), err_got.Error())
 	}
 }
